6b2aec1379d6c845aa5d00005f31f1c50b3c0fec
cjreardon <connorreardon3@gmail.com>
2022-06-27 16:43:41 -0400

    Update and Delete working

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+  const [peakName, setPeakName] = useState("");
+  const [days, setDays] = useState(0);
+  const [newHikeName, setNewName] = useState("");
+  const [peakList, setPeakList] = useState([]);
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/read").then((response) => {
+      setPeakList(response.data);
+    });
+  }, []);
+  const addToList = () => {
+    Axios.post("http://localhost:3001/insert", { peakName, days });
+  };
+
+  const updateName = (id) => {
+    Axios.put("http://localhost:3001/update", {
+      id: id,
+      newHikeName: newHikeName,
+    });
+  };
+
+  const deletePeak = (id) => {
+    Axios.delete(`http://localhost:3001/delete/${id}`)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>PeakBagger</h1>
+      <label>Peak Name</label>
+      <input
+        type="text"
+        onChange={(event) => {
+          setPeakName(event.target.value);
+        }}
+      />
+      <label>Days hiked ago</label>
+      <input
+        type="number"
+        onChange={(event) => {
+          setDays(event.target.value);
+        }}
+      />
+      <button onClick={addToList}>Add to list</button>
+      <h1>Peaks Hiked</h1>
+      {peakList.map((val, key) => {
+        return (
+          <div className="peak">
+            <h1 className="peakName"> Peak Hiked: {val.peakName} </h1>{" "}
+            <h1 className="days"> Hiked {val.daysAgoHiked} Days Ago </h1>{" "}
+            <input
+              type="text"
+              placeholder="Updated hike name"
+              onChange={(event) => {
+                setNewName(event.target.value);
+              }}
+            />
+            <button onClick={() => updateName(val._id)}>Update</button>
+            <button onClick={() => deletePeak(val._id)}>Delete</button>
+          </div>
+        );
+      })}
+      ;
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const PeakModel = require("./models/Peaks");
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(
   "mongodb+srv://cjreardon:rXEJmzJKD7lCV0lI@cluster0.xlf4d.mongodb.net/?retryWrites=true&w=majority",
@@ -12,8 +14,11 @@ mongoose.connect(
   }
 );
 
-app.get("/", async (req, res) => {
-  const peak = new PeakModel({ peakName: "Washington", daysAgoHiked: 3 });
+app.post("/insert", async (req, res) => {
+  const peakName = req.body.peakName;
+  const days = req.body.days;
+
+  const peak = new PeakModel({ peakName: peakName, daysAgoHiked: days });
 
   try {
     await peak.save();
@@ -21,6 +26,37 @@ app.get("/", async (req, res) => {
   } catch (err) {
     console.log(err);
   }
+});
+
+app.put("/update", async (req, res) => {
+  const newPeakName = req.body.newHikeName;
+  const id = req.body.id;
+
+  try {
+    await PeakModel.findById(id, (err, updatedPeak) => {
+      updatedPeak.peakName = newPeakName;
+      updatedPeak.save();
+      res.send("update");
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.get("/read", async (req, res) => {
+  PeakModel.find({}, (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+    res.send(result);
+  });
+});
+
+app.delete("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  await PeakModel.findByIdAndRemove(id).exec();
+
+  res.send("deleted");
 });
 
 app.listen(3001, () => {
